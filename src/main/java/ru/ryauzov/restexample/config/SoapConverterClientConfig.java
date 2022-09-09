2a1c672bba8df705cbf8b08c8787e0bb4805c37e
Pavel Ryauzov <pavel.ryauzov@gmail.com>
2022-09-09 15:11:54 +0300

    soap uri moved to resources

--- src/main/java/ru/ryauzov/restexample/config/SoapConverterClientConfig.java
+++ src/main/java/ru/ryauzov/restexample/config/SoapConverterClientConfig.java
@@ -1,12 +1,21 @@
 package ru.ryauzov.restexample.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import ru.ryauzov.restexample.client.SoapConverterClient;
 
 @Configuration
 public class SoapConverterClientConfig {
+    Environment environment;
+
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public Jaxb2Marshaller marshaller() {
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
@@ -17,7 +26,7 @@ public class SoapConverterClientConfig {
     @Bean
     public SoapConverterClient soapConverterClient(Jaxb2Marshaller marshaller) {
         SoapConverterClient client = new SoapConverterClient();
-        client.setDefaultUri("http://localhost:8080/service");
+        client.setDefaultUri(environment.getProperty("soap.uri"));
         client.setMarshaller(marshaller);
         client.setUnmarshaller(marshaller);
         return client;
